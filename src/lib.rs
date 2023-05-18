dc274831ff5c77322abf31712e162ef72fc9d73c
Peter Jin <>
2023-05-17 21:04:37 -0400

    Checkpoint. It works!

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,26 @@ extern crate home;
 extern crate libc;
 extern crate libloading;
 extern crate rustc_serialize;
+extern crate ryu;
 //extern crate tempfile;
 
 use self::blake2s::{Blake2s};
 
 use home::{home_dir};
 use libc::{c_int, c_void};
-use libloading::os::unix::{Library, Symbol};
+use libloading::nonsafe::{Library, Symbol};
 //use potato::{Blake2s};
 use rustc_serialize::{Decodable};
 use rustc_serialize::hex::{ToHex};
 use rustc_serialize::json::{Decoder as JsonDecoder, Json};
+use ryu::{Buffer as RyuBuffer};
 //use tempfile::{Builder as TempBuilder};
 
+use std::cell::{RefCell};
 use std::collections::{BTreeMap};
 //use std::env;
-use std::fs::{File, create_dir_all};
-use std::io::{Read};
+use std::fs::{File, OpenOptions, create_dir_all};
+use std::io::{Read, Write};
 use std::marker::{PhantomData};
 use std::path::{Path, PathBuf};
 use std::process::{Command};
@@ -27,12 +30,69 @@ use std::ptr::{null_mut};
 use std::str::{from_utf8};
 
 pub mod blake2s;
+pub mod build_env;
 //pub mod temp;
+
+#[derive(Default)]
+pub struct FutharkFloatFormatter {
+  buf:  RefCell<RyuBuffer>,
+}
+
+impl Clone for FutharkFloatFormatter {
+  fn clone(&self) -> FutharkFloatFormatter {
+    FutharkFloatFormatter::default()
+  }
+}
+
+impl FutharkFloatFormatter {
+  pub fn format_f32(&self, x: f32) -> String {
+    let mut s = String::new();
+    self.write_f32(&mut s, x);
+    s
+  }
+
+  pub fn write_f32(&self, s: &mut String, x: f32) {
+    if x.is_nan() {
+      s.push_str("f32.nan");
+    } else if !x.is_finite() {
+      // FIXME FIXME: double check this api.
+      if x.is_sign_negative() {
+        s.push_str("-f32.inf");
+      } else {
+        s.push_str("f32.inf");
+      }
+    } else {
+      s.push_str(self.buf.borrow_mut().format_finite(x));
+      s.push_str("f32");
+    }
+  }
+}
+
+#[derive(Clone, Debug)]
+pub struct Config {
+  // TODO
+  pub work_dir: PathBuf,
+  pub futhark:  PathBuf,
+}
+
+impl Default for Config {
+  fn default() -> Config {
+    let mut work_dir = home_dir().unwrap();
+    work_dir.push(".futhark_ffi");
+    work_dir.push("cache");
+    let futhark = PathBuf::from("../futhark_ffi/futhark");
+    Config{
+      work_dir,
+      futhark,
+    }
+  }
+}
 
 // NB: This "FFI" really just calls the futhark executable.
 
 #[derive(Clone, Copy, Debug)]
 pub enum BuildError {
+  Cache,
   FutharkCommand,
   Futhark(Option<i32>),
   JsonPath,
@@ -150,11 +210,37 @@ pub struct futhark_context_config([u8; 0]);
 #[repr(C)]
 pub struct futhark_context([u8; 0]);
 
+#[allow(non_snake_case)]
 #[derive(Default)]
 pub struct ObjectFFI {
   pub _inner:       Option<Library>,
   pub ctx_cfg_new:  Option<Symbol<extern "C" fn () -> *mut futhark_context_config>>,
   pub ctx_cfg_free: Option<Symbol<extern "C" fn (*mut futhark_context_config)>>,
+  // TODO TODO
+  pub ctx_cfg_set_cuGetErrorString:         Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuInit:                   Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuDeviceGetCount:         Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuDeviceGetName:          Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuDeviceGet:              Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuDeviceGetAttribute:     Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuCtxCreate:              Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuCtxDestroy:             Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuCtxPopCurrent:          Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuCtxPushCurrent:         Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuCtxSynchronize:         Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuMemAlloc:               Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuMemFree:                Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuMemcpy:                 Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuMemcpyHtoD:             Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuMemcpyDtoH:             Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuMemcpyAsync:            Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuMemcpyHtoDAsync:        Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuMemcpyDtoHAsync:        Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cudaEventCreate:          Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cudaEventDestroy:         Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cudaEventRecord:          Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cudaEventElapsedTime:     Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_nvrtcGetErrorString:      Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
   pub ctx_cfg_set_nvrtcCreateProgram:       Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
   pub ctx_cfg_set_nvrtcDestroyProgram:      Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
   pub ctx_cfg_set_nvrtcCompileProgram:      Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
@@ -165,10 +251,19 @@ pub struct ObjectFFI {
   pub ctx_cfg_set_cuModuleLoadData:         Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
   pub ctx_cfg_set_cuModuleUnload:           Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
   pub ctx_cfg_set_cuModuleGetFunction:      Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
-  pub ctx_cfg_set_cuFuncGetAttr:            Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
+  pub ctx_cfg_set_cuFuncGetAttribute:       Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
   pub ctx_cfg_set_cuLaunchKernel:           Option<Symbol<extern "C" fn (*mut futhark_context_config, *mut c_void)>>,
   pub ctx_new:      Option<Symbol<extern "C" fn (*mut futhark_context_config) -> *mut futhark_context>>,
   pub ctx_free:     Option<Symbol<extern "C" fn (*mut futhark_context)>>,
+  pub ctx_set_max_block_size:       Option<Symbol<extern "C" fn (*mut futhark_context, usize)>>,
+  pub ctx_set_max_grid_size:        Option<Symbol<extern "C" fn (*mut futhark_context, usize)>>,
+  pub ctx_set_max_tile_size:        Option<Symbol<extern "C" fn (*mut futhark_context, usize)>>,
+  pub ctx_set_max_threshold:        Option<Symbol<extern "C" fn (*mut futhark_context, usize)>>,
+  pub ctx_set_max_shared_memory:    Option<Symbol<extern "C" fn (*mut futhark_context, usize)>>,
+  pub ctx_set_max_bespoke:          Option<Symbol<extern "C" fn (*mut futhark_context, usize)>>,
+  pub ctx_set_lockstep_width:       Option<Symbol<extern "C" fn (*mut futhark_context, usize)>>,
+  pub ctx_set_device:       Option<Symbol<extern "C" fn (*mut futhark_context, c_int) -> c_int>>,
+  pub ctx_set_stream:       Option<Symbol<extern "C" fn (*mut futhark_context, *mut c_void) -> *mut c_void>>,
   pub ctx_may_fail: Option<Symbol<extern "C" fn (*mut futhark_context) -> c_int>>,
   pub ctx_sync:     Option<Symbol<extern "C" fn (*mut futhark_context) -> c_int>>,
   /*new_f32_1d:   Option<Symbol<extern "C" fn () -> ()>>,
@@ -184,9 +279,10 @@ pub struct ObjectFFI {
 impl Drop for ObjectFFI {
   fn drop(&mut self) {
     let inner = self._inner.take();
-    assert!(inner.is_some());
     *self = Default::default();
-    drop(inner.unwrap());
+    if let Some(inner) = inner {
+      drop(inner);
+    }
   }
 }
 
@@ -195,6 +291,30 @@ impl ObjectFFI {
     let inner = self._inner.as_ref().unwrap();
     self.ctx_cfg_new = inner.get(b"futhark_context_config_new").ok();
     self.ctx_cfg_free = inner.get(b"futhark_context_config_free").ok();
+    self.ctx_cfg_set_cuGetErrorString = inner.get(b"futhark_context_config_set_cuGetErrorString").ok();
+    self.ctx_cfg_set_cuInit = inner.get(b"futhark_context_config_set_cuInit").ok();
+    self.ctx_cfg_set_cuDeviceGetCount = inner.get(b"futhark_context_config_set_cuDeviceGetCount").ok();
+    self.ctx_cfg_set_cuDeviceGetName = inner.get(b"futhark_context_config_set_cuDeviceGetName").ok();
+    self.ctx_cfg_set_cuDeviceGet = inner.get(b"futhark_context_config_set_cuDeviceGet").ok();
+    self.ctx_cfg_set_cuDeviceGetAttribute = inner.get(b"futhark_context_config_set_cuDeviceGetAttribute").ok();
+    self.ctx_cfg_set_cuCtxCreate = inner.get(b"futhark_context_config_set_cuCtxCreate").ok();
+    self.ctx_cfg_set_cuCtxDestroy = inner.get(b"futhark_context_config_set_cuCtxDestroy").ok();
+    self.ctx_cfg_set_cuCtxPopCurrent = inner.get(b"futhark_context_config_set_cuCtxPopCurrent").ok();
+    self.ctx_cfg_set_cuCtxPushCurrent = inner.get(b"futhark_context_config_set_cuCtxPushCurrent").ok();
+    self.ctx_cfg_set_cuCtxSynchronize = inner.get(b"futhark_context_config_set_cuCtxSynchronize").ok();
+    self.ctx_cfg_set_cuMemAlloc = inner.get(b"futhark_context_config_set_cuMemAlloc").ok();
+    self.ctx_cfg_set_cuMemFree = inner.get(b"futhark_context_config_set_cuMemFree").ok();
+    self.ctx_cfg_set_cuMemcpy = inner.get(b"futhark_context_config_set_cuMemcpy").ok();
+    self.ctx_cfg_set_cuMemcpyHtoD = inner.get(b"futhark_context_config_set_cuMemcpyHtoD").ok();
+    self.ctx_cfg_set_cuMemcpyDtoH = inner.get(b"futhark_context_config_set_cuMemcpyDtoH").ok();
+    self.ctx_cfg_set_cuMemcpyAsync = inner.get(b"futhark_context_config_set_cuMemcpyAsync").ok();
+    self.ctx_cfg_set_cuMemcpyHtoDAsync = inner.get(b"futhark_context_config_set_cuMemcpyHtoDAsync").ok();
+    self.ctx_cfg_set_cuMemcpyDtoHAsync = inner.get(b"futhark_context_config_set_cuMemcpyDtoHAsync").ok();
+    self.ctx_cfg_set_cudaEventCreate = inner.get(b"futhark_context_config_set_cudaEventCreate").ok();
+    self.ctx_cfg_set_cudaEventDestroy = inner.get(b"futhark_context_config_set_cudaEventDestroy").ok();
+    self.ctx_cfg_set_cudaEventRecord = inner.get(b"futhark_context_config_set_cudaEventRecord").ok();
+    self.ctx_cfg_set_cudaEventElapsedTime = inner.get(b"futhark_context_config_set_cudaEventElapsedTime").ok();
+    self.ctx_cfg_set_nvrtcGetErrorString = inner.get(b"futhark_context_config_set_nvrtcGetErrorString").ok();
     self.ctx_cfg_set_nvrtcCreateProgram = inner.get(b"futhark_context_config_set_nvrtcCreateProgram").ok();
     self.ctx_cfg_set_nvrtcDestroyProgram = inner.get(b"futhark_context_config_set_nvrtcDestroyProgram").ok();
     self.ctx_cfg_set_nvrtcCompileProgram = inner.get(b"futhark_context_config_set_nvrtcCompileProgram").ok();
@@ -205,10 +325,19 @@ impl ObjectFFI {
     self.ctx_cfg_set_cuModuleLoadData = inner.get(b"futhark_context_config_set_cuModuleLoadData").ok();
     self.ctx_cfg_set_cuModuleUnload = inner.get(b"futhark_context_config_set_cuModuleUnload").ok();
     self.ctx_cfg_set_cuModuleGetFunction = inner.get(b"futhark_context_config_set_cuModuleGetFunction").ok();
-    self.ctx_cfg_set_cuFuncGetAttr = inner.get(b"futhark_context_config_set_cuFuncGetAttr").ok();
+    self.ctx_cfg_set_cuFuncGetAttribute = inner.get(b"futhark_context_config_set_cuFuncGetAttribute").ok();
     self.ctx_cfg_set_cuLaunchKernel = inner.get(b"futhark_context_config_set_cuLaunchKernel").ok();
     self.ctx_new = inner.get(b"futhark_context_new").ok();
     self.ctx_free = inner.get(b"futhark_context_free").ok();
+    self.ctx_set_max_block_size = inner.get(b"futhark_context_set_max_block_size").ok();
+    self.ctx_set_max_grid_size = inner.get(b"futhark_context_set_max_grid_size").ok();
+    self.ctx_set_max_tile_size = inner.get(b"futhark_context_set_max_tile_size").ok();
+    self.ctx_set_max_threshold = inner.get(b"futhark_context_set_max_threshold").ok();
+    self.ctx_set_max_shared_memory = inner.get(b"futhark_context_set_max_shared_memory").ok();
+    self.ctx_set_max_bespoke = inner.get(b"futhark_context_set_max_bespoke").ok();
+    self.ctx_set_lockstep_width = inner.get(b"futhark_context_set_lockstep_width").ok();
+    self.ctx_set_device = inner.get(b"futhark_context_set_device").ok();
+    self.ctx_set_stream = inner.get(b"futhark_context_set_stream").ok();
     self.ctx_may_fail = inner.get(b"futhark_context_may_fail").ok();
     self.ctx_sync = inner.get(b"futhark_context_sync").ok();
     // TODO
@@ -218,25 +347,33 @@ impl ObjectFFI {
 }
 
 pub struct Object<B: Backend> {
-  manifest: ObjectManifest,
-  ffi:  ObjectFFI,
-  cfg:  *mut futhark_context_config,
-  ctx:  *mut futhark_context,
+  pub manifest: ObjectManifest,
+  pub ffi:  ObjectFFI,
+  pub cfg:  *mut futhark_context_config,
+  pub ctx:  *mut futhark_context,
   _mrk: PhantomData<B>,
 }
 
-impl<B: Backend> Object<B> {
-  pub fn cached_or_new(src_buf: &[u8]) -> Result<Object<B>, BuildError> {
+impl<B: Backend> Drop for Object<B> {
+  fn drop(&mut self) {
+    if !self.ctx.is_null() {
+      (self.ffi.ctx_free.as_ref().unwrap())(self.ctx);
+    }
+    if !self.cfg.is_null() {
+      (self.ffi.ctx_cfg_free.as_ref().unwrap())(self.cfg);
+    }
+  }
+}
+
+impl Config {
+  pub fn cached_or_new_object<B: Backend>(&self, src_buf: &[u8]) -> Result<Object<B>, BuildError> {
     let mut srchash = Blake2s::new_hash();
     srchash.hash_bytes(src_buf);
     let h = srchash.finalize();
     let hx = h.to_hex();
     let stem = format!("futhark_obj_{}_{}", B::cmd_arg(), hx);
-    let mut work_dir = home_dir().unwrap();
-    work_dir.push(".futhark_ffi");
-    work_dir.push("cache");
-    create_dir_all(&work_dir).ok();
-    let mut f_path = work_dir.clone();
+    create_dir_all(&self.work_dir).ok();
+    let mut f_path = self.work_dir.clone();
     f_path.push(&stem);
     f_path.set_extension("fut");
     let mut c_path = f_path.clone();
@@ -254,9 +391,17 @@ impl<B: Backend> Object<B> {
       }
     }
     dylib_path.set_extension("so");
-    match Command::new("futhark")
+    // FIXME FIXME: caching.
+    match OpenOptions::new().write(true).create(true).truncate(true).open(&f_path) {
+      Err(_) => return Err(BuildError::Cache),
+      Ok(ref mut f) => {
+        f.write_all(src_buf).unwrap();
+      }
+    }
+    match Command::new(&self.futhark)
       //.arg(from_utf8(B::cmd_arg()).unwrap())
       .arg(B::cmd_arg())
+      .arg("--library")
       .arg(&f_path)
       .status()
     {
@@ -288,15 +433,29 @@ impl<B: Backend> Object<B> {
     if &manifest.backend != B::cmd_arg() {
       panic!("bug");
     }
+    println!("DEBUG: futhark_ffi::Config::cached_or_new_object: target: {}", self::build_env::TARGET);
+    println!("DEBUG: futhark_ffi::Config::cached_or_new_object: dylib path: {}", dylib_path.to_str().unwrap());
+    println!("DEBUG: futhark_ffi::Config::cached_or_new_object: dylib file: {}", dylib_path.file_name().unwrap().to_str().unwrap());
     match cc::Build::new()
-      .shared_flag(true)
-      .static_flag(false)
+      // NB: We have to set `out_dir`, `target`, `host`, `debug`, and `opt_level`;
+      // normally they are read from env vars passed by cargo to the build script.
+      .out_dir(&self.work_dir)
+      .target(self::build_env::TARGET)
+      .host(self::build_env::TARGET)
+      .debug(false)
       .opt_level(2)
-      .include(&work_dir)
+      .pic(true)
+      .include(&self.work_dir)
+      .include("../virtcuda")
       .file(&c_path)
-      .try_compile(dylib_path.to_str().unwrap())
+      .object_prefix_hash(false)
+      .archive(false)
+      .dylib(true)
+      //.try_compile(dylib_path.file_name().unwrap().to_str().unwrap())
+      .try_compile(&stem)
     {
-      Err(_) => {
+      Err(e) => {
+        println!("WARNING: futhark_ffi::Config::cached_or_new_object: cc build error: {}", e);
         return Err(BuildError::Cc);
       }
       Ok(_) => {}
